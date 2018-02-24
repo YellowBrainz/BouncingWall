ef928b30b323d838b302f50ff4f5cadc0add762b
Toon Leijtens <Toon.Leijtens@ing.com>
2018-02-24 16:51:16 +0100

    initial commit

---
+++ app/src/main/java/com/example/smart_t/myapplication/BouncingWall.kt
@@ -0,0 +1,12 @@
+package com.example.smart_t.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BouncingWall : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bouncing_wall)
+    }
+}
